e84cc1ca5d0c51f0b10a74fc6f41a1e4d613e878
sh4869 <nobuk4869@gmail.com>
2016-07-30 00:15:31 +0900

    Create changing Scene Code

---
+++ Assets/Scripts/TitleScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class TitleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("return")){
+			SceneManager.LoadScene("Main");
+		}
+	}
+	void OnGUI(){
+		GUI.Label(new Rect(50,50,100,20), "Game Start!!");
+	}
+}
