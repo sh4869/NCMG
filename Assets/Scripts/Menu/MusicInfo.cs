b3fbd4ec213531a86297976717e0f56587e71e9c
sh4869 <nobuk4869@gmail.com>
2016-08-03 11:17:26 +0900

    ファイル構成の変更

---
+++ Assets/Scripts/Menu/MusicInfo.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class MusicInfo {
+    // Music Title
+    public string musicTitle;
+    
+    // Music File
+    public string musicFilePath;
+
+    public string musicArtPath;
+}
---
+++ Assets/Scripts/Menu/MusicSelectionScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicSelectionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void SetUpWithJson(){
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
